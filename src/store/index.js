63c4ba3404b7f4677edd1b8ef690c8a5f4b37545
Kuroneko-mayuge <kfrmts0706@gmail.com>
2020-10-04 15:17:27 +0900

    ログイン認証の設定だけ

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
@@ -9,6 +10,10 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    login(){
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(google_auth_provider)
+    }
   },
   modules: {
   }
