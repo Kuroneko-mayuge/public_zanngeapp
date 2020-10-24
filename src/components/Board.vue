54d3b8269e357bfe49725dc338375d1e4a8ce2ff
Kuroneko-mayuge <kfrmts0706@gmail.com>
2020-10-24 17:08:38 +0900

    掲示板の素材を作成02

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     掲示板
+    {{ boardTexts.text }}
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   name: 'Board',
+  data: () => ({
+    boardTexts: []
+  }),
+  created: function(){
+    const boarddb = firebase.firestore().collection('board');
+    const _this = this;
+    boarddb.get()
+    .then((querySnapshot) => {
+     querySnapshot.forEach(function(doc){
+        const catchdata = doc.data();
+        _this.boardTexts.push(catchdata);
+    })})
+  }
 }
 </script>
